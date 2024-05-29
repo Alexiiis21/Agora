80393d690470d92b9896dec16cc5c27db4ee48a7
Alexiiis21 <acardenas22@alumnos.uaq.mx>
2024-05-28 18:16:21 -0600

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
@@ -24,4 +25,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
